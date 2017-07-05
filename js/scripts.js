2ef03687149f416f9570c2feecd6d97c3defc877
Katarzyna Lachcik <kaisek@poczta.fm>
2017-07-05 21:14:47 +0200

    tabs

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ function Phone(brand, price, color) {
 };
 
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 
 };
 
-var iPhone6S = new Phone("Apple", 2250, "silver"),
-var samsungGalaxyS6 = new Phone("Samsung", 2000, "black"),
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var samsungGalaxyS6 = new Phone("Samsung", 2000, "black");
 var bV6000 = new Phone("Black View", 900, "orange");
 
 iPhone6S.printInfo();
